82d30fb8f2cd6b4b3075aef977529d9d738ddd9d
Shannon Miller <slmiller99@gmail.com>
2013-11-24 11:37:01 -0500

    Add show and delete functionality

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,15 @@
 class PicsController < ApplicationController
 
+  def show
+    @pic = Pic.find(params[:id])
+  end
+
+  def destroy
+    Pic.destroy(params[:id])
+    redirect_to pics_path
+  end
+
+
   def create
     # Pic.create(:lesson => 1, ...)
     @pic = Pic.create( pic_params )
